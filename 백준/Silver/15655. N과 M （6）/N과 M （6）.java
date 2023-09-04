11175e1770b7e6f937d9b588d023b803229e47a2
MingiNew <119585059+minginew@users.noreply.github.com>
2023-09-04 17:24:13 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int N,M;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] in1 = br.readLine().split(" ");
+		String[] in2 = br.readLine().split(" ");
+		N = Integer.parseInt(in1[0]);
+		M = Integer.parseInt(in1[1]);
+		int[] arr = new int[N];
+		int[] out = new int[N];
+		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(in2[i]);
+		Arrays.sort(arr);
+		back(arr, out, 0, -1, sb);
+		System.out.println(sb.toString());
+		
+	}
+	static void back(int[] arr, int[] out, int depth, int max, StringBuilder sb) {
+		if(depth==M) {
+			for(int i=0; i<N; i++) {
+				if(out[i] != 0) sb.append(out[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<arr.length; i++) {
+			if(i<=max) continue;
+			out[depth] = arr[i];
+			back(arr, out, depth+1, i, sb);
+		}
+		
+	}
+}
