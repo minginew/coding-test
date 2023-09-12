5be7f091a50a897671df4fddd451e6772c9489f7
MingiNew <119585059+minginew@users.noreply.github.com>
2023-09-12 16:02:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -3,27 +3,36 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int[] hobbit = new int[9];
-        int sum = 0;
-        for(int i=0; i<hobbit.length; i++){
-            hobbit[i] = Integer.parseInt(br.readLine());
-            sum += hobbit[i];
-        }
-        Arrays.sort(hobbit);
-        int left=0;
-        int right=8;
-        while (left<right){
-            if(sum-(hobbit[left]+hobbit[right])<100) right--;
-            else if (sum-(hobbit[left]+hobbit[right])>100) left++;
-            else if (sum-(hobbit[left]+hobbit[right])==100) break;
-        }
-        for(int i=0; i<hobbit.length; i++){
-            if(i==left || i==right) continue;
-            sb.append(hobbit[i]).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[9];
+		for(int i=0; i<9; i++) arr[i] = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		int left = 0;
+		int right = arr.length-1;
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) sum += arr[i];
+		int twoSum = sum - 100;
+		
+		while(left<right) {
+			int temp = arr[left]+arr[right];
+			if(twoSum > temp) {
+				left++;
+			}
+			else if(twoSum < temp) {
+				right--;
+			}else {
+				arr[left] = 0;
+				arr[right] = 0;
+				break;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == 0) continue;
+			System.out.println(arr[i]);
+		}
+	}
+
 }
