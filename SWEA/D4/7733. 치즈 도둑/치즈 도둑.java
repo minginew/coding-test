e8c6cc06c58eb5bbd6d35aebaf9696878314885f
MingiNew <119585059+minginew@users.noreply.github.com>
2023-09-19 12:08:33 +0900

    [D4] Title: 치즈 도둑, Time: 486 ms, Memory: 99,196 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -19,7 +19,6 @@ public class Solution {
 			maxChunk=1;
 			map = new int[N][N];
 			
-			
 			for(int r=0; r<N; r++) {//치즈세팅
 				String[] row = br.readLine().split(" ");
 				for(int c=0; c<N; c++) {
@@ -31,38 +30,31 @@ public class Solution {
 				Queue<int[]> q = new LinkedList<>();
 				boolean[][] visit = new boolean[N][N];
 				
-				for(int r=0; r<N; r++) { //i일와 같은 맛에 해당하는 치즈 먹기, 0으로
-					for(int c=0; c<N; c++) {
-						if(map[r][c] == i) map[r][c]=0;
-					}
-				}
-				
 				int chunk = 0;
 				for(int r=0; r<N; r++) {//치즈 덩어리수 계산하기
 					for(int c=0; c<N; c++) {
-						if(map[r][c]==0 || visit[r][c]) continue;
+						if(map[r][c]<=i || visit[r][c]) continue;
 						q.offer(new int[] {r,c});
 						visit[r][c] = true;
-						bfs(q,visit);
+						bfs(q,visit,i);
 						chunk++;
 					}
 				}
 				if(maxChunk<chunk) maxChunk=chunk;
-				
 			}
 			sb.append("#").append(tc).append(" ").append(maxChunk).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static void bfs(Queue<int[]> q, boolean[][] visit) {
+	public static void bfs(Queue<int[]> q, boolean[][] visit, int i) {
 		while(!q.isEmpty()) {
 			int r = q.peek()[0];
 			int c = q.poll()[1];
 			for(int d=0; d<4; d++) {
 				int nr = r+dr[d];
 				int nc = c+dc[d];
-				if(nr<0 || nc<0 || nr>=N || nc>=N || map[nr][nc]==0 || visit[nr][nc]) continue;
+				if(nr<0 || nc<0 || nr>=N || nc>=N || map[nr][nc]<=i || visit[nr][nc]) continue;
 				q.offer(new int[] {nr,nc});
 				visit[nr][nc] = true;
 			}
