ac81fd82d568fcc55606fec5ea77e3724724f9f0
MingiNew <119585059+minginew@users.noreply.github.com>
2023-09-21 15:15:39 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 852 ms, Memory: 118,328 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,87 +1,73 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
-
-class Edge {
-	int i; //섬과
-	int j; //섬을
-	double w; //잇는데 드는 비용
-	
-	public Edge(int i, int j, double w) {
-		super();
-		this.i = i;
-		this.j = j;
-		this.w = w;
-	}
-}
+import java.util.PriorityQueue;
 
 public class Solution {
-	static int idx;
-	static double E;
-	static int[] x,y,p;
+	static class Edge{
+		int ed;
+		double w;
+		
+		public Edge(int ed, double w2) {
+			super();
+			this.ed = ed;
+			this.w = w2;
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int T = Integer.parseInt(br.readLine());
+		
 		for(int t=1; t<=T; t++) {
-			idx = Integer.parseInt(br.readLine());
-			
+			int N = Integer.parseInt(br.readLine());
 			String[] inX = br.readLine().split(" ");
 			String[] inY = br.readLine().split(" ");
-			x = new int[idx];
-			y = new int[idx];
-			p = new int[idx];
-			E = Double.parseDouble(br.readLine());
+			int[] x = new int[N];
+			int[] y = new int[N];
+			double E = Double.parseDouble(br.readLine());
 			
-			for(int i=0; i<idx; i++) x[i] = Integer.parseInt(inX[i]);
-			for(int i=0; i<idx; i++) y[i] = Integer.parseInt(inY[i]);
+			List<Edge>[] list = new ArrayList[N];
+			boolean[] visit = new boolean[N];
 			
-			for(int i=0; i<p.length; i++) make(i);
+			for(int i=0; i<N; i++) x[i] = Integer.parseInt(inX[i]);
+			for(int i=0; i<N; i++) y[i] = Integer.parseInt(inY[i]);
+			for(int i=0; i<N; i++) list[i] = new ArrayList<>();
 			
-			List<Edge> edge = new LinkedList<>();
-			for(int i=0; i<idx; i++) {
-				for(int j=i+1; j<idx; j++) {
-					double w = Math.pow(x[i]-x[j], 2) + Math.pow(y[i]-y[j], 2);
-					edge.add(new Edge(i, j, w));
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(i==j) continue;
+					double w = E*(Math.pow(x[i]-x[j], 2) + Math.pow(y[i]-y[j], 2));
+					list[i].add(new Edge(j, w));
 				}
 			}
 			
-			Collections.sort(edge, new Comparator<Edge>() {
+			PriorityQueue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
 				@Override
 				public int compare(Edge o1, Edge o2) {
-					// TODO Auto-generated method stub
 					return Double.compare(o1.w, o2.w);
 				}
 			});
 			
-			double cost = 0;
-			for(Edge e : edge) {
-				int findX = find(e.i);
-				int findY = find(e.j);
+			pq.addAll(list[0]);
+			visit[0] = true;
+			
+			int cnt = 1;
+			double sum = 0;
+			while(cnt!=N) {
+				Edge e = pq.poll();
 				
-				if(findX != findY) {
-					union(findX, findY);
-					cost += E*e.w;
-				}
+				if(visit[e.ed]) continue;
+				pq.addAll(list[e.ed]);
+				visit[e.ed] = true;
+				sum += e.w;
+				cnt++;
+				if(cnt==N) break;
 			}
-			sb.append("#").append(t).append(" ").append((long)Math.round(cost)).append("\n");
+			sb.append("#").append(t).append(" ").append(Math.round(sum)).append("\n");
 		}
 		System.out.println(sb);
-	}
-	
-	public static void make(int i) {
-		p[i] = i;
-	}
-	public static int find(int x) {
-		if(x != p[x]) 
-			p[x] = find(p[x]);
-		return p[x];
-	}
-	public static void union(int x, int y) {
-		p[y] = x;
 	}
 }
