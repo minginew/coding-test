dcc5e55b9a56c941b77860099638d63ad5ae3342
MingiNew <119585059+minginew@users.noreply.github.com>
2023-09-25 00:21:09 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 104 ms, Memory: 19,728 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -2,39 +2,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Solution {
-	static int N;
-	static int H;
-	static int[] height;
-	static int min;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		for(int t=1; t<=T; t++) {
-			String[] in1 = br.readLine().split(" ");
-			N = Integer.parseInt(in1[0]);
-			H = Integer.parseInt(in1[1]);
-			min = Integer.MAX_VALUE;
-			height = new int[N];
-			String[] in2 = br.readLine().split(" ");
-			for(int i=0; i<N; i++) {
-				height[i] = Integer.parseInt(in2[i]);
-			}
-			combi(0,0);
-			sb.append("#").append(t).append(" ").append(min-H).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
-	
-	static void combi(int idx, int h) {
-		if(idx == N) {
-			if(H<=h && min>h) {
-				min=h;
-			}
-			return;
-		}
-		combi(idx+1, h);
-		combi(idx+1, h+height[idx]);
-	}
+    static int N,H,answer;
+    static int[] height; //null 값 들어가있는 상태
+    static boolean[] visit;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t=1; t<=T; t++){
+            String[] input = br.readLine().split(" ");
+            N = Integer.parseInt(input[0]); //째간이들 수
+            H = Integer.parseInt(input[1]); //선반 높이
+            height = new int[N];
+            visit = new boolean[N];
+            answer = Integer.MAX_VALUE;
+            String[] in = br.readLine().split(" ");
+            for(int i=0; i<N; i++){//째깐이들 키 초기화
+                height[i] = Integer.parseInt(in[i]);
+            }
+
+
+            //부분집합을 지정 -> 뽑은 놈들의 키를 합함 -> H를 넘지 않으면 컷 -> 넘으면 저장 그중 작은놈들 -> 다끝나면
+            // 그값에서 - H
+            subSet(0,0);
+            sb.append("#").append(t).append(" ").append(answer-H).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void subSet(int idx, int sum){
+        if(idx == N){
+            if(sum < H) return;
+            if(answer>sum) answer = sum;
+            return;
+        }
+        subSet(idx+1, sum);
+        visit[idx] = true;
+        subSet(idx+1, sum+height[idx]);
+
+    }
 }
