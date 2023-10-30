e62f5244504f84fd57bf00b8c35ef4ffcf380a1e
MingiNew <119585059+minginew@users.noreply.github.com>
2023-10-30 13:02:29 +0900

    [Gold V] Title: 빗물, Time: 140 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int N,M;
+	static int[][] map;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] in = br.readLine().split(" ");
+		N = Integer.parseInt(in[0]);
+		M = Integer.parseInt(in[1]);
+		map = new int[N][M];
+		
+		String[] rainArr = br.readLine().split(" ");
+		for(int c=0; c<M; c++) {
+			int rain = Integer.parseInt(rainArr[c]);
+			for(int r=N-1; r>=N-rain; r--) {
+				map[r][c] = 1;
+			}
+		}
+		
+		int answer = 0;
+		for(int r=N-1; r>=0; r--) {
+			int left = 0;
+			int right = 0;
+			int count = 0;
+			for(int c=0; c<M; c++) {
+				if(left==0 && right==0) {
+					left = map[r][c];
+				}else if(left==1 && right==0){
+					right = map[r][c];
+					if(right==1) {
+						answer += count;
+						right = 0;
+						count=0;
+						continue;
+					}
+					count++;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+}
