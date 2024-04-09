ffc6ec3fb451b1f6eae2eb7f86dd2694756f45ff
손민기 <thsalsrl7531@naver.com>
2024-04-09 23:46:49 +0900

    :bulb: 1368.물대기 코드정리 및 해결과정 추가

--- 백준/Gold/1368. 물대기/물대기.java
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -4,13 +4,19 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    //논의수
     static int N;
+    //최소비용
     static int minTotal = Integer.MAX_VALUE;
+    //우선순위 큐에 방문했던 간선의 비용정보
     static int[] queueInfo;
+    //논을 파는데 들어가는 비용
     static int[] digCost;
+    //방문노드 배열
     static boolean[] visit;
+    //인접배열 (노드간 비용정보)
     static int[][] adjArr;
-    static List<Integer> startList = new LinkedList<>();
+    
     static class Edge {
         int st;
         int ed;
@@ -60,6 +66,7 @@ public class Main {
 
             for(int j=0; j<N; j++){
                 if(i == j) continue;
+                // index 번째 노드로 들어오는 간선의 비용을 기록해둔다.
                 queueInfo[j] = Math.min(adjArr[i][j], digCost[j]);
                 pq.add(new Edge(i, j, queueInfo[j]));
             }
@@ -68,10 +75,12 @@ public class Main {
                 Edge curr = pq.poll();
                 if(visit[curr.ed]) continue;
                 visit[curr.ed] = true;
+                // 물을 끌어올지, 아니면 새로 논의 우물을 팔지 결정한다.
                 totalCost += Math.min(adjArr[curr.st][curr.ed], digCost[curr.ed]);
 
                 int newSt = curr.ed;
                 for(int newEd=0; newEd<N; newEd++){
+                    // newEd 노드로 들어오는 간선기록을 확인후 불필요한 간선을 걸러준다.
                     if(!visit[newEd] && adjArr[newSt][newEd] <= queueInfo[newEd]){
                         pq.add(new Edge(newSt, newEd, adjArr[newSt][newEd]));
                         queueInfo[newEd] = adjArr[newSt][newEd];
